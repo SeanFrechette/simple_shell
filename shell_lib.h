4fce67b7d93056be7c883954c305cf177ee2fe99
Lucas Lazarre <lucaslazarre@Lucass-MacBook-Air.local>
2021-04-14 20:22:48 -0400

    Adding Built-in functions to the holberton file

--- shell_lib.h
+++ shell_lib.h
@@ -1,6 +1,7 @@
 #ifndef shell_lib
 #define shell_lib
 
+/* Libraries */
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -11,3 +12,9 @@ char *_strncpyconst(char *dest, const char *src, int n);
 unsigned int _strlen_const(const char *name);
 int _strcmp(char *s1, char *s2);
 unsigned int _strlen(char *s);
+
+/* Built-ins */
+void builtin_env(void);
+void builtin_exit(char **cmd, char *b);
+
+#endif
